5a60daa5f9c11d4ee7008d9244bc74a62f29ec93
rfilgas <rfilgas@pdx.edu>
2022-04-11 20:55:55 +0000

    added gather script for trimet data

--- trimet_gather.py
+++ trimet_gather.py
@@ -1,9 +1,14 @@
 import requests
+from datetime import date
+
 
 
 response = requests.get("https://developer.trimet.org/ws/V1/arrivals/locIDs/6849,6850/appID/324C58C9AAA75511C1422AA3E")
-print(response)
-print(response.text)
+
+filename = "./trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
+f = open(filename, "w")
+f.write(response.text)
+
 
 # Servicecalls
 '''
