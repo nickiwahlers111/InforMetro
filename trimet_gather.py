5ce6c6d3c6cbacb0d65ae1811e2932b5322e24e8
rfilgas <rfilgas@pdx.edu>
2022-04-13 20:58:16 +0000

    converted gatherer to python script

--- trimet_gather.py
+++ trimet_gather.py
@@ -1,13 +1,20 @@
+#! /usr/bin/python3
+
 import requests
 from datetime import date
+import sys
 
+def main(args):
 
+    response = requests.get("https://developer.trimet.org/ws/V1/arrivals/locIDs/6849,6850/appID/324C58C9AAA75511C1422AA3E")
 
-response = requests.get("https://developer.trimet.org/ws/V1/arrivals/locIDs/6849,6850/appID/324C58C9AAA75511C1422AA3E")
+    filename = "/home/rfilgas/InforMetro/trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
+    #filename = "./trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
+    f = open(filename, "w")
+    f.write(response.text)
 
-filename = "./trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
-f = open(filename, "w")
-f.write(response.text)
+if __name__ == '__main__':
+    main(sys.argv)
 
 
 # Servicecalls
