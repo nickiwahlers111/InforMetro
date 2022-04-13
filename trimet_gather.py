ef34764da233adfa5a10cd4f5bc67cd6ef9393c0
Clara Stickney <cstick2@pdx.edu>
2022-04-13 14:18:46 -0700

    Changed to relative pathing in gatherer script

--- trimet_gather.py
+++ trimet_gather.py
@@ -3,12 +3,15 @@
 import requests
 from datetime import date
 import sys
+import os
 
 def main(args):
 
+    username = os.getlogin()
+
     response = requests.get("https://developer.trimet.org/ws/V1/arrivals/locIDs/6849,6850/appID/324C58C9AAA75511C1422AA3E")
 
-    filename = "/home/rfilgas/InforMetro/trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
+    filename = "/home/" + username + "/InforMetro/trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
     #filename = "./trimet_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
     f = open(filename, "w")
     f.write(response.text)
