f259e2d92d178c33c4909bc545e7e38cafd337e0
nickiwahlers111 <nwahlers@project-instance.c.dotted-virtue-339520.internal>
2022-04-15 21:39:29 +0000

    create directory if DNE

--- ctran_gather.py
+++ ctran_gather.py
@@ -9,6 +9,10 @@ def main(args):
 
     username = os.getlogin()
     response = requests.get("http://www.psudataeng.com:8000/getBreadCrumbData")
+    path = "/home/" + username + "/InforMetro/ctran_data/" 
+    exists = os.path.exists(path)
+    if not exists:
+      os.makedirs(path)
     filename = "/home/" + username + "/InforMetro/ctran_data/" + date.today().strftime('%m-%d-%Y') + ".txt"
     f = open(filename, "w")
     f.write(response.text)
