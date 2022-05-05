fb6eff04a365514a7cbe6cf2cf40cdff43a651d3
nickiwahlers111 <nwahlers@project-instance.c.dotted-virtue-339520.internal>
2022-05-05 22:27:30 +0000

    refactored database stuff, now with three methods. Examples of how to call in database_tester.py

--- database_tester.py
+++ database_tester.py
@@ -1,9 +1,27 @@
-from database import database_stuff
+#! /usr/bin/python3
 
-dtrip = {'trip_id':[169472306],'route_id':[100],'vehicle_id':[100],'service_key':['Sunday'],'direction':['Out']}
-dbread = {'tstamp':["2022-05-04 14:30:25"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472306] }
+from database2 import *
+
+dtrip1 = {'trip_id':[169472306],'route_id':[100],'vehicle_id':[100],'service_key':['Sunday'],'direction':['Out']}
+dtrip2 = {'trip_id':[169472307],'route_id':[100],'vehicle_id':[100],'service_key':['Sunday'],'direction':['Out']}
+
+dbread1 = {'tstamp':["2022-05-04 14:30:25"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472306] }
+dbread2 = {'tstamp':["2022-05-04 14:30:25"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472307] }
+
+breadframe = []
+breadframe.append(dbread1)
+breadframe.append(dbread2)
+tripframe = []
+tripframe.append(dtrip1)
+tripframe.append(dtrip2)
+
 print("Calling database_stuff!\n")
-database_stuff(dtrip, dbread)
+
+conn = open_and_create()
+for i in range(2):
+  insert_into_table(conn, tripframe[i], breadframe[i])
+
+close_db(conn)
 
 #TODO: currently the data tables get dropped if exist every time database_stuff is called,
 #can we call it once outisde of loop in ctran_consumer file?
