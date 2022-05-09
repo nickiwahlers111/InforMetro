efee81e87bbb3c2c10b17513f60716f3ab79b989
nickiwahlers111 <nwahlers@project-instance.c.dotted-virtue-339520.internal>
2022-05-09 20:21:36 +0000

    updated database work and added utility

--- database_tester.py
+++ database_tester.py
@@ -3,22 +3,26 @@
 from database import *
 
 dtrip1 = {'trip_id':[169472306],'route_id':[100],'vehicle_id':[100],'service_key':['Sunday'],'direction':['Out']}
-dtrip2 = {'trip_id':[169472307],'route_id':[100],'vehicle_id':[100],'service_key':['Sunday'],'direction':['Out']}
+dtrip2 = {'trip_id':[169472307],'route_id':[100],'vehicle_id':[101],'service_key':['Sunday'],'direction':['Out']}
+dtrip3 = {'trip_id':[169472308],'route_id':[100],'vehicle_id':[101],'service_key':['Sunday'],'direction':['Out']}
 
 dbread1 = {'tstamp':["2022-05-04 14:30:25"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472306] }
-dbread2 = {'tstamp':["2022-05-04 14:30:25"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472307] }
+dbread2 = {'tstamp':["2022-05-04 14:00:00"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [11], 'speed':[11], 'trip_id':[169472307] }
+dbread3 = {'tstamp':["2022-05-04 15:00:00"], 'latitude': [45.631987], 'longitude': [-122.640735], 'direction': [100], 'speed':[11], 'trip_id':[169472307] }
 
 breadframe = []
 breadframe.append(dbread1)
 breadframe.append(dbread2)
+breadframe.append(dbread3)
 tripframe = []
 tripframe.append(dtrip1)
 tripframe.append(dtrip2)
+tripframe.append(dtrip3)
 
 print("Calling database_stuff!\n")
 
 conn = open_and_create()
-for i in range(2):
+for i in range(3):
   insert_into_table(conn, tripframe[i], breadframe[i])
 
 close_db(conn)
